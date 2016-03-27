5d3130e1ca15a60bcbab0e9e6c842d7c558bf154
Andrew Tulloch <andrew@tullo.ch>
2016-03-27 13:54:33 -0500

    [Caffe] Optional NNPACK integration
    The approach is pretty straightforward and very similar to the CuDNN integration:
    
    - Add some CMake magic to detect NNPACK if it's installed on the user's
      system.
    - Add some Makefile.config options
    - Add some thread-local state (a threadpool for NNPACK to caffe::Caffe).
    - Add an NNPACK `Engine` parameter to Convolution/Pooling/InnerProduct
      layers.
    - In LayerFactory, ifdef around whether NNPACK is enabled and dispatch
      to NNPACK*Layer or *Layer depending on the Engine parameter.
    - In NNPACK*Layer, ensure the NNPACK preconditions hold and call the
      appropriate nnpack_*  function, otherwise fall back to *Layer.
    
    I'm not sure how common it is to be running CNN inference on Haswell and
    beyond CPUs for Caffe users, but if it is it might be a nice boost for
    them.

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -58,6 +58,19 @@ void GlobalInit(int* pargc, char*** pargv) {
 #endif
 }
 
+#ifdef USE_NNPACK
+  template<>
+  bool Caffe::nnpack_supported<double>() {
+    return false;
+  }
+
+  template<>
+  bool Caffe::nnpack_supported<float>() {
+    static enum nnp_status nnpack_status = nnp_initialize();
+    return nnpack_status == nnp_status_success;
+  }
+#endif
+
 #ifdef CPU_ONLY  // CPU-only Caffe.
 
 Caffe::Caffe()
