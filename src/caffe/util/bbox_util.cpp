bfb3a8200c9e41ce5afe84c10ec0150e3940a9ee
James Leigh <james_a_leigh@yahoo.co.uk>
2016-05-18 15:31:39 +0100

    to compile on windows after merge

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -12,6 +12,10 @@
 
 #include "caffe/util/bbox_util.hpp"
 
+# if defined(_MSC_VER) && _MSC_VER < 1900
+# define snprintf _snprintf_s
+# endif
+
 namespace caffe {
 
 bool SortBBoxAscend(const NormalizedBBox& bbox1, const NormalizedBBox& bbox2) {
